f311ff71aa86262f6b7c62c822fba91d82c27ea6
s1131225 <lucyamy23780983@gmail.com>
2025-12-01 14:39:57 +0800

    v1_陳芯霈

--- app/src/main/java/tw/edu/pu/csim/tcyang/s1131225/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/s1131225/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import tw.edu.pu.csim.tcyang.s1131225.ui.theme.S1131225Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,21 +22,25 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
                     )
                 }
             }
         }
     }
 }
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.TopStart)
     )
 }
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
